910201d82339dce7bb8f00ad8d907acac2b0926c
goran-erhartic-devtech <goran.erhartic@devtechgroup.com>
2017-11-15 13:17:42 +0100

    Added confirmation step for DM's approve/decline action

--- src/helpers/ApproveLeaveMessage.php
+++ src/helpers/ApproveLeaveMessage.php
@@ -32,12 +32,20 @@ class ApproveLeaveMessage
 		$action1['type'] = "button";
 		$action1['value'] = true;
 		$action1['style'] = "primary";
+		$action1['confirm']['title'] = "Please confirm";
+		$action1['confirm']['text'] = "Are you sure you want to approve this leave?";
+		$action1['confirm']['ok_text'] = "Yup";
+		$action1['confirm']['dismiss_text'] = "Nope";
 
 		$action2['name'] = "button";
 		$action2['text'] = "Decline";
 		$action2['type'] = "button";
 		$action2['value'] = false;
 		$action2['style'] = "danger";
+		$action2['confirm']['title'] = "Please confirm";
+		$action2['confirm']['text'] = "Are you sure you want to decline this leave?";
+		$action2['confirm']['ok_text'] = "Yup";
+		$action2['confirm']['dismiss_text'] = "Nope";
 
 		$attachment['actions'] = array($action1, $action2);
 		$payload['attachments'][] = $attachment;
